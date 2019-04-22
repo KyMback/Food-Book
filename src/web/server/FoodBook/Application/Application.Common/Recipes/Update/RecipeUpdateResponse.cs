e1fde48f4b2eeab79f5d9f498bb01afd91d3be52
KyMback <dark_AHrel@mail.ru>
2019-04-22 21:37:31 +0300

    fix updating response

--- src/web/server/FoodBook/Application/Application.Common/Recipes/Update/RecipeUpdateResponse.cs
+++ src/web/server/FoodBook/Application/Application.Common/Recipes/Update/RecipeUpdateResponse.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace FoodBook.Application.Common.Recipes.Update
 {
     public class RecipeUpdateResponse
     {
+        public Guid Id { get; set; }
         
+        public string Title { get; set; }
+
+        public string Ingredients { get; set; }
     }
 }
